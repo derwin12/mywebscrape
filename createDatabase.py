5da9eaa833c8d522268e8d8623d7dd697b4345f0
Daryl Erwin <derwin@bbmtek.com>
2022-03-19 14:29:38 -0400

    Scrape Google Drive

--- createDatabase.py
+++ createDatabase.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('sequence.db')
 c = conn.cursor()
 
-c.execute('''DROP TABLE sequences''')
+c.execute('''DROP TABLE IF EXISTS sequences''')
 c.execute('''CREATE TABLE sequences(storename TEXT, url TEXT, name TEXT, last_seen DATE)''')
 
 c.close()
