e51542fdf087c771ba4b488aee910cd207bb4d56
MrPaulAR <pwthoma@philzy.com>
2022-03-19 14:33:42 -0500

    Bostik initial run

---
+++ bostik.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+import httpx
+from urllib.parse import urljoin
+from dataclasses import dataclass
+
+BASEURL = "https://bostickfamilylightshow.com/collections/sequences"
+
+
+@dataclass
+class Sequence:
+    name: str
+    url: str
+
+
+def get_products_from_page(soup: BeautifulSoup) -> list[Sequence]:
+
+    products = soup.find_all(
+        "div", class_="grid__item small--one-half medium-up--one-fifth"
+    )
+
+    sequences = []
+    for product in products:
+        sequence_name = product.find(class_="product-card__name").text
+        # song, artist = sequence_name.split(" - ")
+        product_url = urljoin(BASEURL, product.find("a")["href"])
+        sequences.append(Sequence(sequence_name, product_url))
+
+    next_page = soup.find(class_="next")
+    if next_page:
+        next_page_url = urljoin(BASEURL, next_page.find("a")["href"])  # type: ignore
+        response = httpx.get(next_page_url)
+        next_soup = BeautifulSoup(response.text, "html.parser")
+        sequences.append(get_products_from_page(next_soup))
+
+    return sequences
+
+
+def main() -> None:
+
+    response = httpx.get(BASEURL)
+    soup = BeautifulSoup(response.text, "html.parser")
+    products = get_products_from_page(soup)
+
+    print(products)
+
+
+if __name__ == "__main__":
+    main()
