b709699330e8ac3b2ed993f148d1b1e7329c73f5
Daryl Erwin <derwin@bbmtek.com>
2022-03-20 12:04:15 -0400

    Basic Webserver

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    con = sqlite3.connect('sequence.db')
+    cur = con.cursor()
+    cur.execute("SELECT * FROM sequences")
+    rows = cur.fetchall()
+
+    return render_template('sequences.html', title='Sequences list',
+                           rows=rows)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080, host='0.0.0.0')
