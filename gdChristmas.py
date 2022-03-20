2a691b300a132d16df60168b3a867f801de3b413
Daryl Erwin <derwin@bbmtek.com>
2022-03-20 17:51:58 -0400

    Scrape Google Drive Christmas Dir

---
+++ gdChristmas.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+from getfilelistpy import getfilelist
+from googleapiclient.discovery import build
+from insertData import insSequence
+
+load_dotenv()  # take environment variables from .env.
+
+API_KEY = os.getenv("API_KEY", "Missing API_KEY")
+FOLDER_ID = os.getenv("FOLDER_ID", "Missing FOLDER_ID")
+service = build('drive', 'v3', developerKey=API_KEY)
+
+items = []
+pageToken = ""
+
+resource = {
+    "api_key": API_KEY,
+    "id": FOLDER_ID,
+    "fields": "files(name,id)",
+}
+
+
+def main() -> None:
+    res = getfilelist.GetFileList(resource)
+
+    for key, value in res.items():
+        if key == 'fileList':
+            for a in sorted(value[0]["files"], key=lambda x: x["name"]):
+                if a["name"].endswith("zip"):
+                    try:
+                        insSequence(store="GoogleDrive", url="https://drive.google.com/uc?id=" +
+                                                             a["id"] +
+                                                             "&amp;authuser=0&amp;export=download",
+                                    name=a["name"].split("--")[0] + " -- " +
+                                    a["name"].split("--")[1] + " (" +
+                                    a["name"].split("--")[2].split(".")[0] + ")"
+                                    )
+                    except:
+                        pass
+
+
+if __name__ == "__main__":
+    main()
