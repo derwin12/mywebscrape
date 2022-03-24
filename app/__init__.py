64cc0aed029f15cd2d1f3a2d1b31998a39436a3c
Daryl Erwin <derwin@bbmtek.com>
2022-03-24 14:52:57 -0400

    Adding base urls tied to a vendor

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import exc
 
 app = Flask(__name__, instance_relative_config=True)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sequences.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+STRING_FAIL = 'fail'
+STRING_SUCCESS = 'success'
+
 
 class Vendor(db.Model):  # type: ignore
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     name = db.Column(db.String(120), unique=True, index=True, nullable=False)
-    urls = db.relationship("Base_Url", backref="vendor", lazy=True)
-    sequences = db.relationship("Sequence", backref="vendor", lazy=True)
+    urls = db.relationship('Base_Url', backref='vendor')
 
     def __repr__(self):
         return f"<Vendor %r>" % self.name
+
+
+class Base_Url(db.Model):  # type: ignore
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    url = db.Column(db.String, nullable=False, unique=True)
+    vendor_id = db.Column(db.Integer, db.ForeignKey("vendor.id"), nullable=False)
+
+    def __repr__(self):
+        return f"<Base_Url %r>" % self.url
 
 
 class Sequence(db.Model):  # type: ignore
@@ -27,29 +39,21 @@ class Sequence(db.Model):  # type: ignore
         return f"<Sequence %r>" % self.name
 
 
-class Base_Url(db.Model):  # type: ignore
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    url = db.Column(db.String, nullable=False, unique=True)
-    vendor_id = db.Column(db.Integer, db.ForeignKey("vendor.id"))
-
-    def __repr__(self):
-        return f"<Base_Url %r>" % self.url
-
-
 @app.route("/")
 def index():
-    return render_template("main.html")
+#   return render_template("main.html")
     # con = sqlite3.connect("sequence.db")
     # cur = con.cursor()
     # cur.execute("SELECT * FROM sequences ORDER BY storename, name")
     # rows = cur.fetchall()
 
-    # return render_template("sequences.html", title="Sequences list", rows=rows)
+    return render_template("sequence.html", title="Find A Sequence")
 
 
 @app.route("/vendors")
 def vendors():
-    return render_template("vendors.html")
+    v = Vendor.query.all()
+    return render_template("vendors.html", vendors=v)
 
 
 @app.route("/register-vendor", methods=["POST"])
@@ -57,18 +61,39 @@ def register_vendor():
     form = request.form
     vendor = Vendor(name=form["name"])
     db.session.add(vendor)
-    db.session.commit()
-    return "Successfully added vendor"
+    try:
+        session_commit()
+        return "Successfully added vendor"
+    except exc.IntegrityError:
+        db.session.rollback()
+        return "Error"
 
 
-@app.route("/register-url", methods=["POST"])
+#@login_required
+@app.route("/register-url", methods=["GET", "POST"])
 def register_url():
-    return "Form Submitted"
+    if request.method == 'POST':
+        form = request.form
+        if form["url"] and form["vendor_id"]:
+            base_url = Base_Url(url=form["url"], vendor_id=form["vendor_id"])
+            db.session.add(base_url)
+            db.session.commit()
+            return f"{base_url.url} successfully created."
+        else:
+            return "Missing values."
+    else:
+        v = Vendor.query.all()
+        u = Base_Url.query.all()
+    return render_template('urls.html', vendors=v, base_urls=u)
 
 
 @app.route("/search", methods=["GET", "POST"])
 def sequence():
-    # if request.method == "POST":
+    if request.method == "POST":
+        print("Searching...")
+        id = 1
+        res = Sequence.query.get(id)
+        print("Results", res)
     #     con = sqlite3.connect("sequence.db")
     #     cur = con.cursor()
 
@@ -77,11 +102,11 @@ def sequence():
     #         + "where lower(name) like lower(?) ORDER BY storename, name",
     #         ("%{}%".format(request.form["search_string"]),),
     #     )
-    #     return render_template(
-    #         "sequence.html",
-    #         title="Found Sequences (" + request.form["search_string"] + ")",
-    #         records=cur.fetchall(),
-    #     )
+        return render_template(
+             "sequence.html",
+             title="Found Sequences (" + request.form["search_string"] + ")",
+             records={},
+         )
 
     return render_template("sequence.html", title="Find A Sequence")
 
@@ -89,6 +114,16 @@ def sequence():
 app.config.from_object("config")
 
 
-if "__main__" == __name__:
+def session_commit():
+    try:
+        db.session.commit()
+        return jsonify(meta = STRING_SUCCESS)
+    except exc.IntegrityError:
+        print("IntegrityError while adding new user")
+        db.session.rollback()
+        return jsonify(meta = STRING_FAIL)
+
+
+if __name__ == 'app':
     db.create_all()
     app.run(debug=True, port=8080)
--- my_funcs/__init__.py
+++ my_funcs/__init__.py
@@ -4,6 +4,15 @@ from dataclasses import dataclass
 
 from sqlalchemy import create_engine
 
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+from app import Sequence
+
+app = Flask(__name__, instance_relative_config=True)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sequences.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 @dataclass
 class Sequence:
@@ -12,21 +21,23 @@ class Sequence:
 
 
 def insSequence(store, url, name):
-    conn = sqlite3.connect("sequence.db")
-    c = conn.cursor()
-    current_date = datetime.datetime.now()
+#    conn = sqlite3.connect("c:\Users\elcrapamundo\PycharmProjects\app\sequences.db")
+#    c = conn.cursor()
+ #   current_date = datetime.datetime.now()
 
-    c.execute(
-        """DELETE from sequences where url=? AND storename = ?""",
-        (
-            url,
-            store,
-        ),
-    )
-    c.execute(
-        """INSERT into sequences VALUES(?,?,?,?)""", (store, url, name, current_date)
-    )
-    conn.commit()
+  #  c.execute(
+   #     """DELETE from sequences where url=? AND storename = ?""",
+    #    (
+     #       url,
+      #      store,
+#        ),
+ #   )
+  #  c.execute(
+   #     """INSERT into sequences VALUES(?,?,?,?)""", (store, url, name, current_date)
+    sequence = Sequence(name, url)
+    db.session.add(sequence)
+    print(sequence)
+    db.session.commit()
 
     c.close()
 
