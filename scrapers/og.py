17bfc1a827c77c325f529bf81854fe2d9e23432e
MrPaulAR <MrPaulAR@gmail.com@users.noreply.github.com>
2022-04-16 08:57:10 -0500

    Added secondary query for price

--- scrapers/og.py
+++ scrapers/og.py
@@ -3,13 +3,11 @@ from dataclasses import dataclass
 from urllib.parse import urljoin
 
 import httpx
+from app import BaseUrl, Vendor
 from bs4 import BeautifulSoup
-
 from my_funcs import insert_sequence
 
-from app import BaseUrl, Vendor
-
-storename = 'OG Sequences'
+storename = "OG Sequences"
 
 
 @dataclass
@@ -19,16 +17,32 @@ class Sequence:
     price: str
 
 
+def get_price_from_product_page(product_url: str) -> str:
+    response = httpx.get(product_url, follow_redirects=True)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    prices = soup.find_all("meta", itemprop="price")
+    price = max([float(i["content"]) for i in prices])
+    return f"${price:.2f}"
+
+
 def get_products_from_page(soup: BeautifulSoup, url: str) -> list[Sequence]:
 
     products = soup.find_all("div", class_="elementor-image")
 
     sequences = []
     for product in products:
-        sequence_name = product.find(class_="widget-image-caption wp-caption-text").text.strip(' *')
+        sequence_name = product.text.replace("*", "").strip()
         product_url = urljoin(url, product.find("a")["href"])
-# TODO: need to drill thru for pricing
-        price = "$50.00"
+
+        if "ogsequences" not in product_url:
+            seq_num = re.search(".*p=(\d+).*", product_url).group(1)  # type: ignore
+            product_url = (
+                f"http://ogsequences.com/?post_type=download&p={seq_num}&preview=true"
+            )
+
+        # Price is on a secondary page.
+        price = get_price_from_product_page(product_url)
         sequences.append(Sequence(sequence_name, product_url, price))
 
     next_page = soup.find(class_="next")
@@ -42,8 +56,13 @@ def get_products_from_page(soup: BeautifulSoup, url: str) -> list[Sequence]:
 
 def main() -> None:
     print(f"Loading %s" % storename)
-    baseurls = BaseUrl.query.join(Vendor).add_columns(Vendor.name.label("vendor_name")) \
-        .filter(Vendor.name == storename).order_by(BaseUrl.id).all()
+    baseurls = (
+        BaseUrl.query.join(Vendor)
+        .add_columns(Vendor.name.label("vendor_name"))
+        .filter(Vendor.name == storename)
+        .order_by(BaseUrl.id)
+        .all()
+    )
     for baseurl in baseurls:
         print(f"Loading %s" % baseurl[0].url)
         response = httpx.get(baseurl[0].url)
@@ -51,7 +70,9 @@ def main() -> None:
         products = get_products_from_page(soup, baseurl[0].url)
 
         for product in products:
-            insert_sequence(store=storename, url=product.url, name=product.name, price=product.price)
+            insert_sequence(
+                store=storename, url=product.url, name=product.name, price=product.price
+            )
 
 
 if __name__ == "__main__":
