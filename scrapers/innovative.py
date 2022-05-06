f4fe1c0071932395f28675bd3fd1dbee3dd0bbe9
MrPaulAR <MrPaulAR@gmail.com@users.noreply.github.com>
2022-05-06 14:33:39 -0500

    Added Vendor class

--- scrapers/innovative.py
+++ scrapers/innovative.py
@@ -1,14 +1,17 @@
 from urllib.parse import urljoin
 
 import httpx
-from app import Sequence
+from app import Sequence, Vendor
 from bs4 import BeautifulSoup
 from my_funcs import create_or_update_sequences, get_unique_vendor
 
 storename = "Innovative Sequences"
 
 
-def get_products_from_page(soup: BeautifulSoup, url: str) -> list[Sequence]:
+def get_products_from_page(
+    soup: BeautifulSoup, url: str, vendor: Vendor
+) -> list[Sequence]:
+
     products = soup.find_all("li", class_="grid__item")
 
     sequences = []
@@ -21,8 +24,11 @@ def get_products_from_page(soup: BeautifulSoup, url: str) -> list[Sequence]:
             price = product.find(class_="price-item--regular").text.strip()
         if price == "$0":
             price = "Free"
-        sequences.append(Sequence(name=sequence_name, link=product_url, price=price))
-
+        sequences.append(
+            Sequence(
+                name=sequence_name, vendor_id=vendor.id, link=product_url, price=price
+            )
+        )
     return sequences
 
 
@@ -34,9 +40,9 @@ def main() -> None:
         print(f"Loading {url.url}")
         response = httpx.get(url.url, timeout=30.0)
         soup = BeautifulSoup(response.text, "html.parser")
-        products = get_products_from_page(soup, url.url)
+        sequences = get_products_from_page(soup=soup, url=url.url, vendor=vendor)
 
-        create_or_update_sequences(products)
+        create_or_update_sequences(sequences)
 
 
 if __name__ == "__main__":
