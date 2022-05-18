65a6a1bf8dee58fb67164c907bfd87b5a4deb6d9
Daryl Erwin <derwin@bbmtek.com>
2022-05-18 10:06:26 -0400

    New UXSG file

--- scrapers/uxsg.py
+++ scrapers/uxsg.py
@@ -8,7 +8,8 @@ from openpyxl import load_workbook
 storename = "UXSG"
 BASEURL = "https://www.facebook.com/groups/228562318717861/search/?q="
 
-filename = "Unofficial xLights Sharing Group Shared Files List (31 Jan 2022).xlsx"
+#filename = "Unofficial xLights Sharing Group Shared Files List (31 Jan 2022).xlsx"
+filename = "Unofficial xLights Sharing Group Shared Files List (17 May 2022).xlsx"
 name_column = 6
 artist_column = 7
 author_column = 9
@@ -18,15 +19,19 @@ def main() -> None:
     print("Loading UXSG Spreadsheet")
     vendor = get_unique_vendor(storename)
 
-    dir = os.getcwd() + "\\..\\app\\Data\\"
+    if os.name != 'posix':
+        dir = os.getcwd() + "\\..\\app\\Data\\"
+    else:
+        dir = os.getcwd() + "//..//app//Data//"
     wb = load_workbook(filename=dir + filename, data_only=True)
-    sheet_obj = wb.active
+    sheet_obj = wb["Shares"]
     sequences = []
 
-    for row in range(5, 1000):
+    for row in range(4, 1000):
         if (
             sheet_obj.cell(column=2, row=row).value
             and "_NA" not in sheet_obj.cell(column=name_column, row=row).value
+            and '#VALUE!' not in sheet_obj.cell(column=name_column, row=row).value
         ):
             # exlude the NA ones
             sequence_name = (
