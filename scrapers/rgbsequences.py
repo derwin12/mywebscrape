679f8d2e404c38cc94cd4bd415aa93d04c3b8d78
Daryl Erwin <derwin@bbmtek.com>
2023-02-07 10:33:15 -0500

    New rgb-sequences store

--- scrapers/rgbsequences.py
+++ scrapers/rgbsequences.py
@@ -1,5 +1,6 @@
 from urllib.parse import urljoin
 
+import re
 import httpx
 from app import Sequence, Vendor
 from bs4 import BeautifulSoup
@@ -13,22 +14,25 @@ def get_products_from_page(
     soup: BeautifulSoup, url: str, vendor: Vendor
 ) -> list[Sequence]:
 
-    products = soup.find_all("li", class_="type-product")
+    products = soup.find_all("div", class_="grid__item small--one-half medium-up--one-fifth")
     sequences = []
     for product in products:
-        sequence_name = product.find(class_="woocommerce-loop-product__title").text
+        sequence_name = product.find(class_="product-card__name").text
         product_url = urljoin(url, product.find("a")["href"])
-        price = product.find("bdi").text
-        if price == "$0.00":
+        price_str = product.find(class_="product-card__price")
+        price = re.findall(r".*\$([0-9\.]+).*", price_str.text)[0]
+        if price == "$0.00" or price == "0":
             price = "Free"
         sequences.append(
             Sequence(
                 name=sequence_name, vendor_id=vendor.id, link=product_url, price=price
             )
         )
-    next_page = soup.find(class_="next")
+        print(sequence_name, product_url, price)
+    next_page = soup.find("span", class_="next")
     if next_page:
-        response = httpx.get(next_page["href"], timeout=30.0)  # type: ignore
+        next_url = urljoin(url, next_page.find("a")["href"])
+        response = httpx.get(next_url, timeout=30.0)  # type: ignore
         next_soup = BeautifulSoup(response.text, "html.parser")
         sequences.extend(get_products_from_page(soup=next_soup, url=url, vendor=vendor))
 
@@ -45,7 +49,7 @@ def main() -> None:
         soup = BeautifulSoup(response.text, "html.parser")
         sequences = get_products_from_page(soup=soup, url=url.url, vendor=vendor)
 
-        create_or_update_sequences(sequences)
+        #create_or_update_sequences(sequences)
 
 
 if __name__ == "__main__":
