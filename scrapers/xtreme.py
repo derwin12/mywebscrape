7f6ea4caab3cd5aeb34494a1bc445590c9476aa6
Daryl Erwin <derwin@bbmtek.com>
2023-03-01 12:37:21 -0500

    xTreme changes in layout .. lol.

--- scrapers/xtreme.py
+++ scrapers/xtreme.py
@@ -30,8 +30,10 @@ def get_products_from_page(
                 name=sequence_name, vendor_id=vendor.id, link=product_url, price=price
             )
         )
-    next_page = soup.find(class_="next")
-    if next_page:
+# #elPagination_b1ae5437920375879c1ec73544e75a71_1184440054 > li.ipsPagination_next
+    next_page_anchor = soup.find_all("li", class_="ipsPagination_next")
+    if next_page_anchor and not soup.find_all("li", class_="ipsPagination_next ipsPagination_inactive"):
+        next_page = soup.find("a", attrs={"rel":"next"})
         response = httpx.get(next_page["href"], timeout=30.0)  # type: ignore
         next_soup = BeautifulSoup(response.text, "html.parser")
         sequences.extend(get_products_from_page(soup=next_soup, url=url, vendor=vendor))
