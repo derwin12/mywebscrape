dff5dd843f9037391be813483f6fe8059fa175fa
Daryl Erwin <derwin@bbmtek.com>
2023-07-12 19:58:20 -0400

    Add renamed vendor

--- scrapers/ils.py
+++ scrapers/ils.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 from my_funcs import create_or_update_sequences, get_unique_vendor
 
 
-storename = "Jolly Jingle Sequences"
+storename = "Illusion Light Shows"
 page = 1
 
 
